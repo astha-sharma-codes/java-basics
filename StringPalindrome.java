e6ec68f8ef272a0a0e9bb21ca79d42f1a9f953a8
ASTHA SHARMA <btbts24063_astha@banasthali.in>
2026-01-14 17:51:11 +0530

    Added string palindrome checker

---
+++ StringPalindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringPalindrome
+  {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) 
+        {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+        {
+            System.out.println("The string is a Palindrome");
+        } else 
+        {
+            System.out.println("The string is NOT a Palindrome");
+        }
+    }
+}
